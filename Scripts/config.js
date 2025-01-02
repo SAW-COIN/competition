6ecddd068746c1500b1a5e9c927766cec57f049d
elsoghiar <mahmoudhouseen555@gmail.com>
2025-01-02 09:50:19 +0200

    Update config.js

--- Scripts/config.js
+++ Scripts/config.js
@@ -3,26 +3,8 @@ function decode(encoded) {
     return atob(encoded);
 }
 
-const encryptedUrlParts = [
-    "aHR0cHM6Ly9sbnh5",
-    "anRwbnZvd2JwdGJv",
-    "bnpodC5zdXBhYmFz",
-    "ZS5jbw=="
-];
+const encryptedUrl = "aHR0cHM6Ly9sbnh5anRwbnZvd2JwdGJvbnpodC5zdXBhYmFzZS5jbw==";
+const encryptedKey = "ZXlKaGJHY2lPaUpJVXpJMU5pSXNJbVZ0WVdsc0lqb2lNREF3TWpVd05ETTJPVGswTWprNU5qRTJOamMxSWl3aWFXRjBJam94TmpReU9UZ3dOell3ZlEuekp0d2IxNEZRdlJyT01sc1ZxelJSZUZTd3VKNjZIWjRZX1RxMER2bTVJcw==";
 
-const encryptedKeyParts = [
-    "ZXlKaGJHY2lPaUpJ",
-    "VXpJMU5pSXNJbVZ0",
-    "WVdsc0lqb2lNREF3",
-    "TWpVd05ETTJPVGsw",
-    "TWprNU5qRTJOamMx",
-    "SWl3aWFXRjBJam94",
-    "TmpReU9UZ3dOell3",
-    "ZlEuekp0d2IxNEZR",
-    "dlJyT01sc1ZxelJS",
-    "ZUZTd3VKNjZIWjRZ",
-    "X1RxMER2bTVJcw=="
-];
-
-export const SUPABASE_URL = decode(encryptedUrlParts.join(""));
-export const SUPABASE_ANON_KEY = decode(encryptedKeyParts.join(""));
+export const SUPABASE_URL = decode(encryptedUrl);
+export const SUPABASE_ANON_KEY = decode(encryptedKey);
