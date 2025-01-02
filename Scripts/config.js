abcbab77466761c5d9a00c8eeaa40107b30a5915
elsoghiar <mahmoudhouseen555@gmail.com>
2025-01-02 10:01:53 +0200

    Update config.js

--- Scripts/config.js
+++ Scripts/config.js
@@ -1,10 +1,6 @@
-// config.js
-function decode(encoded) {
-    return atob(encoded);
-}
-
+// فك التشفير باستخدام js-base64
 const encryptedUrl = "aHR0cHM6Ly9sbnh5anRwbnZvd2JwdGJvbnpodC5zdXBhYmFzZS5jbw==";
 const encryptedKey = "ZXlKaGJHY2lPaUpJVXpJMU5pSXNJbVZ0WVdsc0lqb2lNREF3TWpVd05ETTJPVGswTWprNU5qRTJOamMxSWl3aWFXRjBJam94TmpReU9UZ3dOell3ZlEuekp0d2IxNEZRdlJyT01sc1ZxelJSZUZTd3VKNjZIWjRZX1RxMER2bTVJcw==";
 
-export const SUPABASE_URL = decode(encryptedUrl);
-export const SUPABASE_ANON_KEY = decode(encryptedKey);
+const SUPABASE_URL = Base64.decode(encryptedUrl);
+const SUPABASE_ANON_KEY = Base64.decode(encryptedKey);
