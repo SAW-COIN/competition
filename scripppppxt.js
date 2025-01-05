0c3c5b0a6936f2d80de6946b6875ce1fc3893f51
elsoghiar <mahmoudhouseen555@gmail.com>
2025-01-05 19:55:56 +0200

    Update scripppppxt.js

--- scripppppxt.js
+++ scripppppxt.js
@@ -393,7 +393,7 @@ tonConnectUI.uiOptions = {
 
 async function makePayment() {
     try {
-        const requiredAmount = '50000'; 
+        const requiredAmount = '500000000'; 
         const walletAddress = 'UQBOBIEGLWuaMNLBy3HTaYU-F-3Py8q7o0kGw7S_2vLxRmqr';
 
         const transaction = {
@@ -424,7 +424,7 @@ document.getElementById("payNow").addEventListener("click", makePayment);
 // عرض مستويات VIP في الواجهة
 function renderVIPLevels() {
     const vipLevels = [
-        { id: 1, name: "VIP Silver", price: 0.000005, features: "Basic perks and increased chances." },
+        { id: 1, name: "VIP Silver", price: 5, features: "Basic perks and increased chances." },
         { id: 2, name: "VIP Gold", price: 20, features: "Enhanced perks and increased chances." },
         { id: 3, name: "VIP Platinum", price: 40, features: "Premium perks and maximum chances." },
     ];
